f00f2baa448cdb1c711b7da5dbdfe1427d407c73
indrek <indrek.runkaru@khk.ee>
2019-09-25 10:43:44 +0300

    teksti väljastus koos erinevate html elementidega

---
+++ x_1.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * eesnimi
+ * perenimi
+ * email
+ * vanus
+ * pikkus m
+ * kaal kg:
+ *
+ */
+
+print "Indrek<br>";
+print "rünkaru<br>";
+print "indrek93@gmail.com<br>";
+print "26<br>";
+print "1.84m<br>";
+print "72kg<br>";
+

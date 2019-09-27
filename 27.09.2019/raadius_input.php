2482b41327251680c7309d71e5952f92ca5684d6
indrek <indrek.runkaru@khk.ee>
2019-09-27 10:00:10 +0300

    teksti väljastus koos erinevate html elementidega

--- 27.09.2019/raadius_input.php
+++ 27.09.2019/raadius_input.php
@@ -10,15 +10,17 @@ echo 'sisestatud raadius on' .$raadius.'<br>';
 
 // arvutused
 //ringi pindala
-$ringiPindala = 3.14 * 3.14 * $raadius;
+$ringiPindala = pi() * pi() * $raadius;
+$ringiPindakaUmmardatud= round($ringiPindala, 3);
 //ümbermõõt
-$ringiumbermoot = 3.14 * 2 * $raadius;
+$ringiumbermoot = pi()* 2 * $raadius;
 //muutujate andmete tüüp ja väärtuste kontroll
 /*var_dump($ringiPindala);
 var_dump($ringiumbermoot);
 var_dump($raadius);*/
 //väljastame tulemused
-echo 'sisestatud ringi pindala on' .$ringiPindala.'<br>';
+echo 'sisestatud ringi pindala ummardatud' .$ringiPindakaUmmardatud.'<br>';
+echo 'sisestatud ringi pindala on' .round($ringiPindala,5).'<br>';
 echo 'sisestatud ringi ümbermõõt on' .$ringiumbermoot.'<br>';
 echo 'sisestatud ringi raadius on' .$raadius.'<br>';
 echo 'sisestatud ringi pindala on' .$ringiPindala.'<br>';

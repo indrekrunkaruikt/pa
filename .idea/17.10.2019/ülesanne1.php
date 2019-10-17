39ffbf57c44af74076cb88a6f61b47a92c11a9e8
indrek <indrek.runkaru@khk.ee>
2019-10-17 09:02:09 +0300

    Kodused ülesanded 17.10.2019 viimased parandused.

---
+++ .idea/17.10.2019/ülesanne1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+for
+
+
+($count =1; $count <=20;  $count++){
+    echo $count. '&nbsp';
+    if($count <10){
+        echo $co
+    }
+    echo '* esimene rida';
+    echo '<br>';
+
+
+}
+
+?>

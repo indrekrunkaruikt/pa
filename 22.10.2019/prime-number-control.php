c0d44b796a5203391d942a3ff73d4d95c23e7944
indrek <indrek.runkaru@khk.ee>
2019-10-24 13:19:34 +0300

    ülesanded

--- 22.10.2019/prime-number-control.php
+++ 22.10.2019/prime-number-control.php
@@ -6,31 +6,30 @@ function isPrimeNumber($number) {
     if ($number < 2) {
         echo $number.' - antud arv ei kuulu valitud vahemiku sisse <br>';
     } else {
-        $divider = 2; // define divider
+        $divider = 2;
         while ($number % $divider != 0) {
-            // while number is not divided by divider with modulo 0
-            $divider++; // get the next divider value
+
+            $divider++;
         }
-        // if number and divider is equal - prime number
         if ($number == $divider) {
             $result = true;
         } else {
-            // otherwise
+
             $result = false;
         }
     }
     return $result;
 }
-// define function, which use the boolean result of isPrimeNumber and
-// show the text - is prime or is not prime with the number value
+
+
 function isPrimeNumberDescription($number, $isPrime) {
-//  if($iisPrime == true) is the same
+
     if($isPrime) {
         echo $number.' on algarv<br>';
     } else {
         echo $number.' ei ole algarv<br>';
     }
 }
-// use the function
-$number = rand(1, 10);
+
+$number = rand(1, 1000);
 isPrimeNumberDescription($number, isPrimeNumber($number));
